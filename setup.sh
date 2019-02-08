554e02b174387ce139417c7fbca85d2a53dae8a4
David Kimura <dkimura@pivotal.io>
2019-02-08 12:43:39 -0800

    Add vundle to setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ git clone https://github.com/dgkimura/configs.git
 cd configs
 
 cp .vimrc ~/.vimrc
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
 cp .tmux.conf ~/.tmux.conf
